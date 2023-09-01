8747458be10e07348d0eb1259677b853c32cd5c4
SkyLake <70795425+SkyLake-git@users.noreply.github.com>
2023-09-02 01:51:57 +0900

    `Vec3d`の基本関数追加

--- src/main/java/com/lyrica0954/mineleft/mc/math/Vec3d.java
+++ src/main/java/com/lyrica0954/mineleft/mc/math/Vec3d.java
@@ -20,6 +20,10 @@ public class Vec3d {
 		this.z = 0;
 	}
 
+	public static Vec3d zero() {
+		return new Vec3d(0, 0, 0);
+	}
+
 	@Override
 	public String toString() {
 		return String.format("Vec3d(x=%f,y=%f,z=%f)", this.x, this.y, this.z);
@@ -29,7 +33,43 @@ public class Vec3d {
 		return new Vec3d(this.x + x, this.y + y, this.z + z);
 	}
 
+	public Vec3d addVector(Vec3d vec) {
+		return this.add(vec.x, vec.y, vec.z);
+	}
+
 	public Vec3d subtract(double x, double y, double z) {
 		return new Vec3d(this.x - x, this.y - y, this.z - z);
+	}
+
+	public Vec3d subtractVector(Vec3d vec) {
+		return new Vec3d(this.x - vec.x, this.y - vec.y, this.z - vec.z);
+	}
+
+	public Vec3d copy() {
+		return new Vec3d(this.x, this.y, this.z);
+	}
+
+	public double lengthSquared() {
+		return this.x * this.x + this.y * this.y + this.z * this.z;
+	}
+
+	public Vec3d normalize() {
+		double d = this.lengthSquared();
+
+		if (d < 1.0e-5) {
+			return zero();
+		}
+
+		double len = Math.sqrt(d);
+
+		return new Vec3d(this.x / len, this.y / len, this.z / len);
+	}
+
+	public double length() {
+		return Math.sqrt(this.lengthSquared());
+	}
+
+	public Vec3d multiply(double v) {
+		return new Vec3d(this.x * v, this.y * v, this.z * v);
 	}
 }
