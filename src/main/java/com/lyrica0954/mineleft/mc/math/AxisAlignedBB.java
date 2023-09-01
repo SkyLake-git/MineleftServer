512162a957cb5561b622d1a032af6bb637ade561
SkyLake <70795425+SkyLake-git@users.noreply.github.com>
2023-09-02 01:51:46 +0900

    `AxisAlignedBB`の基本関数追加

--- src/main/java/com/lyrica0954/mineleft/mc/math/AxisAlignedBB.java
+++ src/main/java/com/lyrica0954/mineleft/mc/math/AxisAlignedBB.java
@@ -34,6 +34,143 @@ public class AxisAlignedBB {
 		this.maxZ = 0;
 	}
 
+	public AxisAlignedBB copy() {
+		return new AxisAlignedBB(this.minX, this.minY, this.minZ, this.maxX, this.maxY, this.maxZ);
+	}
+
+	public AxisAlignedBB offset(double x, double y, double z) {
+		this.minX += x;
+		this.minY += y;
+		this.minZ += z;
+		this.maxX += x;
+		this.maxY += y;
+		this.maxZ += z;
+
+		return this;
+	}
+
+	@Override
+	public String toString() {
+		return String.format("AxisAlignedBB(minX=%f,minY=%f,minZ=%f,maxX=%f,maxY=%f,maxZ=%f)", this.minX, this.minY, this.minZ, this.maxX, this.maxY, this.maxZ);
+	}
+
+	public AxisAlignedBB addCoord(double x, double y, double z) {
+		if (x < 0) {
+			this.minX += x;
+		} else if (x > 0) {
+			this.maxX += x;
+		}
+
+		if (y < 0) {
+			this.minY += y;
+		} else if (y > 0) {
+			this.maxY += y;
+		}
+
+		if (z < 0) {
+			this.minZ += z;
+		} else if (z > 0) {
+			this.maxZ += z;
+		}
+
+		return this;
+	}
+
+	public AxisAlignedBB offsetCopy(double x, double y, double z) {
+		return this.copy().offset(x, y, z);
+	}
+
+	public AxisAlignedBB contract(double x, double y, double z) {
+		this.minX += x;
+		this.minY += y;
+		this.minZ += z;
+		this.maxX -= x;
+		this.maxY -= y;
+		this.maxZ -= z;
+
+		return this;
+	}
+
+	public double calculateXOffset(AxisAlignedBB bb, double x) {
+		if (bb.maxY <= this.minY || bb.minY >= this.maxY) {
+			return x;
+		}
+		if (bb.maxZ <= this.minZ || bb.minZ >= this.maxZ) {
+			return x;
+		}
+
+		if (x > 0 && bb.maxX <= this.minX) {
+			double x1 = this.minX - bb.maxX;
+			if (x1 < x) {
+				x = x1;
+			}
+		} else if (x < 0 && bb.minX >= this.maxX) {
+			double x2 = this.maxX - bb.minX;
+			if (x2 > x) {
+				x = x2;
+			}
+		}
+
+		return x;
+	}
+
+	public double calculateYOffset(AxisAlignedBB bb, double y) {
+		if (bb.maxX <= this.minX || bb.minX >= this.maxX) {
+			return y;
+		}
+		if (bb.maxZ <= this.minZ || bb.minZ >= this.maxZ) {
+			return y;
+		}
+
+		if (y > 0 && bb.maxY <= this.minY) {
+			double y1 = this.minY - bb.maxY;
+			if (y1 < y) {
+				y = y1;
+			}
+		} else if (y < 0 && bb.minY >= this.maxY) {
+			double y2 = this.maxY - bb.minY;
+			if (y2 > y) {
+				y = y2;
+			}
+		}
+
+		return y;
+	}
+
+	public double calculateZOffset(AxisAlignedBB bb, double z) {
+		if (bb.maxX <= this.minX || bb.minX >= this.maxX) {
+			return z;
+		}
+		if (bb.maxY <= this.minY || bb.minY >= this.maxY) {
+			return z;
+		}
+
+		if (z > 0 && bb.maxZ <= this.minZ) {
+			double z1 = this.minZ - bb.maxZ;
+			if (z1 < z) {
+				z = z1;
+			}
+		} else if (z < 0 && bb.minZ >= this.maxZ) {
+			double z2 = this.maxZ - bb.minZ;
+			if (z2 > z) {
+				z = z2;
+			}
+		}
+
+		return z;
+	}
+
+	public boolean intersectsWith(AxisAlignedBB bb) {
+		double epsilon = 0.0001d;
+		if (bb.maxX - this.minX > epsilon && this.maxX - bb.minX > epsilon) {
+			if (bb.maxY - this.minY > epsilon && this.maxY - bb.minY > epsilon) {
+				return bb.maxZ - this.minZ > epsilon && this.maxZ - bb.minZ > epsilon;
+			}
+		}
+
+		return false;
+	}
+
 	public AxisAlignedBB read(ByteBuf buf) throws Exception {
 		this.minX = buf.readDouble();
 		this.minY = buf.readDouble();
