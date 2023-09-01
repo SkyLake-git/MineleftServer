5fb182bbc1506f3e140ee45623477a8ba86f5337
SkyLake <70795425+SkyLake-git@users.noreply.github.com>
2023-09-02 01:52:30 +0900

    エンティティローテーションを追加

---
+++ src/main/java/com/lyrica0954/mineleft/mc/math/EntityRot.java
@@ -0,0 +1,21 @@
+package com.lyrica0954.mineleft.mc.math;
+
+public class EntityRot {
+
+	public float yaw;
+
+	public float pitch;
+
+	public EntityRot() {
+		this.yaw = 0;
+		this.pitch = 0;
+	}
+
+	public float getYaw() {
+		return yaw;
+	}
+
+	public float getPitch() {
+		return pitch;
+	}
+}
