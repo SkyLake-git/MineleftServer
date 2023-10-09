2957ce0b56cec7d19fd7948340f18535f172f3c3
SkyLake <70795425+SkyLake-git@users.noreply.github.com>
2023-10-10 02:51:13 +0900

    `MathHelper::equals`実装

--- src/main/java/com/lyrica0954/mineleft/utils/MathHelper.java
+++ src/main/java/com/lyrica0954/mineleft/utils/MathHelper.java
@@ -13,4 +13,12 @@ public class MathHelper {
 	public static double clamp(double num, double min, double max) {
 		return num < min ? min : (Math.min(num, max));
 	}
+
+	public static boolean equals(double a, double b, double epsilon) {
+		return Math.abs(a - b) < epsilon;
+	}
+
+	public static boolean equals(double a, double b) {
+		return equals(a, b, 0.00000001d);
+	}
 }
